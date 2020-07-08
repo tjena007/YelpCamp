d61192c1643b99ae109c4ca39ecf0b9b5b57d2af
Tanmay Jena <tjena2@gmail.com>
2020-07-08 12:50:38 +0530

    added navbars, styled campgrounds and forms page

--- app.js
+++ app.js
@@ -9,32 +9,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var campGrounds = [
   {
-    name: "kite",
+    name: "Tso Moriri Lake,Ladakh",
     image:
       "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
   },
   {
-    name: "runner",
+    name: "Camp Exotica, Kullu",
     image:
       "https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
   },
   {
-    name: "lol",
+    name: "Rishikesh Valley camp, Rishikesh",
     image:
       "https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
   },
   {
-    name: "yo",
+    name: "Kipling Camp, Madhya Pradesh",
     image:
       "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
   },
   {
-    name: "kitab",
+    name: "West Ladakh Camp, Ladakh",
     image:
       "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
   },
   {
-    name: "dawg",
+    name: "Nameri Eco Camp, Assam",
     image:
       "https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
   },
